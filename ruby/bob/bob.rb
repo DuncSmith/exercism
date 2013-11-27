554d5b8c57a730ee4652e0d557fbf90b0e1c9d73
Trevor Barnett <trevorbarnett@nhs.net>
2013-11-27 17:18:00 +0000

    :construction_worker: Ruby Bob wip

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -6,10 +6,14 @@ class Bob
   end
   
   def shouting?(something)
-    something == something.upcase
+    saying_something_other_than_numbers?(something) and something == something.upcase
   end
   
   def question?(something)
     something.end_with? '?'
+  end
+  
+  def saying_something_other_than_numbers?(something)
+    something =~ /[A-Za-z]/
   end
 end
