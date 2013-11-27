f748abd6ad566e06b70917ce17c1de4c9669b8d5
Trevor Barnett <mr.ullet@gmail.com>
2013-11-27 22:00:46 +0000

    :construction_worker: Ruby Bob alternate

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,24 +1,60 @@
-class Bob
-  def hey(something)
-    return 'Woah, chill out!' if shouting?(something)
-    return 'Sure.' if question?(something)
-    return 'Fine. Be that way!' if silence?(something)
-    'Whatever.'
-  end
-  
-  def shouting?(something)
-    saying_something_other_than_numbers?(something) and something == something.upcase
-  end
-  
-  def question?(something)
-    something.end_with? '?'
+class Sentence
+  def initialize(the_words)
+    @the_words = the_words
   end
 
-  def silence?(something)
-    something.strip.empty?
+  def self.of(the_words)
+    Sentence.new(the_words)
   end
-  
-  def saying_something_other_than_numbers?(something)
-    something =~ /[A-Za-z]/
+
+  def is_shouted?
+    is_something_other_than_numbers? and is_all_upper_case?
+  end
+
+  def is_a_question?
+    the_words.end_with? '?'
+  end
+
+  def is_silent?
+    the_words.strip.empty?
+  end
+
+  private
+
+  attr_reader :the_words
+
+  def is_something_other_than_numbers?
+    the_words =~ /[A-Za-z]/
+  end
+
+  def is_all_upper_case?
+    the_words == the_words.upcase
+  end
+end
+
+class Response
+  def initialize(sentence_in_response_to)
+    @sentence = sentence_in_response_to
+  end
+
+  def self.to(sentence)
+    Response.new(sentence)
+  end
+
+  def say
+    return 'Woah, chill out!' if sentence.is_shouted?
+    return 'Sure.' if sentence.is_a_question?
+    return 'Fine. Be that way!' if sentence.is_silent?
+    'Whatever.'
+  end
+
+  private
+
+  attr_reader :sentence
+end
+
+class Bob
+  def hey(something)
+    Response.to(Sentence.of(something)).say
   end
 end
