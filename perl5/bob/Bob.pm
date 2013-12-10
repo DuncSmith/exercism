19e2021a2f0b1d9a5791a3b696a9a027a0ff2e32
Trevor Barnett <mr.ullet@gmail.com>
2013-12-10 21:52:55 +0000

    :bike: perl5 bob - using 'trick' of putting private subs as lexical variables

--- perl5/bob/Bob.pm
+++ perl5/bob/Bob.pm
@@ -2,19 +2,22 @@ package Bob;
 
 use strict;
 
+my ($is_shouted, $is_question, $is_silence);
+my ($contains_alpha, $is_upper_case, $ends_with);
+
 sub hey
 {
     my $message = shift;
 
-    if (is_shouted($message))
+    if ($is_shouted->($message))
     {
         'Woah, chill out!';
     }
-    elsif (is_question($message))
+    elsif ($is_question->($message))
     {
         'Sure.';
     }
-    elsif (is_silence($message))
+    elsif ($is_silence->($message))
     {
         'Fine. Be that way!';
     }
@@ -24,47 +27,47 @@ sub hey
     }
 }
 
-sub is_shouted
+$is_shouted = sub
 {
     my $message = shift;
 
-    contains_alpha($message) && is_upper_case($message);
-}
+    $contains_alpha->($message) && $is_upper_case->($message);
+};
 
-sub is_question
+$is_question = sub
 {
     my $message = shift;
 
-    ends_with($message, '?')
-}
+    $ends_with->($message, '?')
+};
 
-sub is_silence
+$is_silence = sub
 {
     my $message = shift;
 
     $message =~ /^\s*$/;
-}
+};
 
-sub contains_alpha
+$contains_alpha = sub
 {
     my $message = shift;
 
     $message =~ /[A-Z]/i;
-}
+};
 
-sub is_upper_case
+$is_upper_case = sub
 {
     my $message = shift;
 
     $message eq uc($message);
-}
+};
 
-sub ends_with
+$ends_with = sub
 {
     my $string = shift;
     my $sub_string = shift;
 
     substr($string, -1, length($sub_string)) eq $sub_string;
-}
+};
 
 1;
