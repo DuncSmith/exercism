cf1e148a68449c14079f44d436d7adc0ba71a5d9
Trevor Barnett <mr.ullet@gmail.com>
2013-12-10 19:55:43 +0000

    :bike: perl5 bob - all but silence

--- perl5/bob/Bob.pm
+++ perl5/bob/Bob.pm
@@ -2,8 +2,58 @@ package Bob;
 
 use strict;
 
-sub hey(message)
+sub hey
 {
+    my $message = shift;
+
+    if (is_shouted($message))
+    {
+        'Woah, chill out!';
+    }
+    elsif (is_question($message))
+    {
+        'Sure.'
+    }
+    else
+    {
+        'Whatever.';
+    }
+}
+
+sub is_shouted
+{
+    my $message = shift;
+
+    contains_alpha($message) && is_upper_case($message);
+}
+
+sub contains_alpha
+{
+    my $message = shift;
+
+    $message =~ /[A-Z]/i;
+}
+
+sub is_upper_case
+{
+    my $message = shift;
+
+    $message eq uc($message);
+}
+
+sub is_question
+{
+    my $message = shift;
+
+    ends_with($message, '?')
+}
+
+sub ends_with
+{
+    my $string = shift;
+    my $sub_string = shift;
+
+    substr($string, -1, length($sub_string)) eq $sub_string;
 }
 
 1;
