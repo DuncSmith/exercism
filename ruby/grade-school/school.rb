f7dbe6a971bed82011edd60283ddb491a953c874
Trevor Barnett <mr.ullet@gmail.com>
2013-12-17 17:21:16 +0000

    Where's this going?

--- ruby/grade-school/school.rb
+++ ruby/grade-school/school.rb
@@ -4,31 +4,11 @@ class School
   end
 
   def add(student_name, grade_number)
-    students.add student_name, grade_number
+    grade(grade_number) << student_name
   end
 
   def grade(grade_number)
-    students.grade grade_number
-  end
-
-  def sort
-    students.sort
-  end
-
-  private
-
-  def students
-    @students ||= Students.new
-  end
-end
-
-class Students < Hash
-  def add(student_name, grade_number)
-    (self[grade_number] ||= []) << student_name
-  end
-
-  def grade(grade_number)
-    self[grade_number] || []
+    students[grade_number] ||= []
   end
 
   def sort
@@ -37,11 +17,15 @@ class Students < Hash
 
   private
 
+  def students
+    @students ||= {}
+  end
+
   def sort_grade(grade_number)
     [grade_number, grade(grade_number).sort]
   end
 
   def sorted_grade_numbers
-    self.keys.sort
+    students.keys.sort
   end
 end
