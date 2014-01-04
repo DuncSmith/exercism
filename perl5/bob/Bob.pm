2d36df2503fc01529a827aefaee441746da280d9
Trevor Barnett <mr.ullet@gmail.com>
2014-01-04 16:12:31 +0000

    :bike: perl5 bob - using shift directly in regex expressions removing need for the short lived local variables.

--- perl5/bob/Bob.pm
+++ perl5/bob/Bob.pm
@@ -7,18 +7,18 @@ use feature 'lexical_subs'; # requires Perl 5.18
 my sub is_shouted; my sub is_question; my sub is_silence;
 my sub contains_alpha; my sub is_upper_case;
 
-sub hey
-{
+sub hey 
+{ 
     my $message = shift;
 
+    return 'Fine. Be that way!' if is_silence($message);
     return 'Woah, chill out!' if is_shouted($message);
     return 'Sure.' if is_question($message);
-    return 'Fine. Be that way!' if is_silence($message);
     return 'Whatever.';
 }
 
-sub is_shouted
-{
+sub is_shouted 
+{ 
     my $message = shift;
 
     is_words($message) && is_upper_case($message);
@@ -26,30 +26,22 @@ sub is_shouted
 
 sub is_question
 {
-    my $message = shift;
-
-    $message =~ /\?$/;
+    shift =~ /\?$/;
 }
 
 sub is_silence
 {
-    my $message = shift;
-
-    $message =~ /^\s*$/;
+    shift =~ /^\s*$/;
 }
 
 sub is_words
 {
-    my $message = shift;
-
-    $message =~ /\p{Letter}/;
+    shift =~ /\p{Letter}/;
 }
 
 sub is_upper_case
 {
-    my $message = shift;
-
-    $message eq uc($message);
+    shift !~ /\p{Lowercase}/;
 }
 
 1;
