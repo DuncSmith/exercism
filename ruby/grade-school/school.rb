b73d7946aac663a80425cd96cf34109e19234ce4
Trevor Barnett <mr.ullet@gmail.com>
2014-01-05 12:25:19 +0000

    :construction_worker: Ruby Grade-school - Students in own class and not externally mutable

--- ruby/grade-school/school.rb
+++ ruby/grade-school/school.rb
@@ -1,18 +1,47 @@
 class School
   def db
-    students
+    students.to_hash
   end
 
   def add(student_name, grade_number)
-    students_by_grade(grade_number) << student_name
+    students.add(student_name, grade_number)
   end
 
   def grade(grade_number)
-    students_by_grade(grade_number).clone
+    students.by_grade(grade_number)
   end
 
   def sort
-    Hash[sorted_grade_numbers.map { |grade_number| sort_grade(grade_number) }]
+    Hash[students.grade_numbers.map { |grade_number| sort_grade(grade_number) }]
+  end
+
+  private
+
+  def students
+    @students ||= Students.new
+  end
+
+  def sort_grade(grade_number)
+    [grade_number, grade(grade_number).sort]
+  end
+end
+
+class Students
+  def add(student_name, grade_number)
+    grade(grade_number) << student_name
+  end
+
+  def by_grade(grade_number)
+    (students[grade_number] || []).clone
+  end
+
+  def grade_numbers
+    students.keys.sort
+  end
+
+  def to_hash
+    # clone/dup is shallow copy but need deeper clone of child arrays aswell
+    Hash[grade_numbers.map { |grade_number| clone_grade(grade_number) }]
   end
 
   private
@@ -21,15 +50,11 @@ class School
     @students ||= {}
   end
 
-  def students_by_grade(grade_number)
+  def grade(grade_number)
     students[grade_number] ||= []
   end
 
-  def sort_grade(grade_number)
-    [grade_number, students_by_grade(grade_number).sort]
-  end
-
-  def sorted_grade_numbers
-    students.keys.sort
+  def clone_grade(grade_number)
+    [grade_number, by_grade(grade_number)]
   end
 end
