86936ea611f336124351cc58cb7848fb9d2d35d9
Trevor Barnett <mr.ullet@gmail.com>
2014-01-17 13:11:17 +0000

    :construction_worker: Ruby Grade-school - exercism.io spec update - School class updated to match

--- ruby/grade-school/grade_school_test.rb
+++ ruby/grade-school/grade_school_test.rb
@@ -12,32 +12,39 @@ class SchoolTest < MiniTest::Unit::TestCase
   end
 
   def test_an_empty_school
-    assert_equal({}, school.db)
+    assert_equal({}, school.to_hash)
   end
 
   def test_add_student
     school.add("Aimee", 2)
-    assert_equal({2 => ["Aimee"]}, school.db)
+    assert_equal({2 => ["Aimee"]}, school.to_hash)
   end
 
   def test_add_more_students_in_same_class
-    school.add("James", 2)
     school.add("Blair", 2)
+    school.add("James", 2)
     school.add("Paul", 2)
-    assert_equal({2 => ["James", "Blair", "Paul"]}, school.db)
+    assert_equal({2 => ["Blair", "James", "Paul"]}, school.to_hash)
   end
 
   def test_add_students_to_different_grades
     school.add("Chelsea", 3)
     school.add("Logan", 7)
-    assert_equal({3 => ["Chelsea"], 7 => ["Logan"]}, school.db)
+    assert_equal({3 => ["Chelsea"], 7 => ["Logan"]}, school.to_hash)
   end
 
   def test_get_students_in_a_grade
+    school.add("Bradley", 5)
+    school.add("Franklin", 5)
+    school.add("Jeff", 1)
+    assert_equal ["Bradley", "Franklin"], school.grade(5)
+  end
+
+  def test_get_students_sorted_in_a_grade
     school.add("Franklin", 5)
     school.add("Bradley", 5)
     school.add("Jeff", 1)
-    assert_equal ["Franklin", "Bradley"], school.grade(5)
+    assert_equal ["Bradley", "Franklin"], school.grade(5)
   end
 
   def test_get_students_in_a_non_existant_grade
@@ -54,29 +61,29 @@ class SchoolTest < MiniTest::Unit::TestCase
       4 => ["Christopher", "Jennifer"],
       6 => ["Kareem"]
     }
-    assert_equal sorted, school.sort
-    assert_equal [3, 4, 6], school.sort.keys
+    assert_equal sorted, school.to_hash
+    assert_equal [3, 4, 6], school.to_hash.keys
   end
 
   def test_get_students_in_a_non_existant_grade_does_not_mutate_db
     # empty db should stay empty
-    assert_equal({}, school.db)
+    assert_equal({}, school.to_hash)
     school.grade(2)
-    assert_equal({}, school.db)
+    assert_equal({}, school.to_hash)
   end
 
   def test_cannot_externally_mutate_empty_internal_db
-    school.db[2] = ["William", "Samantha"]
+    school.to_hash[2] = ["William", "Samantha"]
     assert_equal [], school.grade(2)
   end
 
   def test_distinct_db_everytime
-    refute_same school.db, school.db
+    refute_same school.to_hash, school.to_hash
   end
 
   def test_cannot_externally_mutate_non_empty_internal_db
     school.add('William', 2)
-    school.db[2] << 'Samantha';
+    school.to_hash[2] << 'Samantha';
     assert_equal ['William'], school.grade(2)
   end
 end
