1b0c634dcd3fcf88f430d112ccfddc08cc289364
Trevor Barnett <mr.ullet@gmail.com>
2014-01-17 13:11:17 +0000

    :construction_worker: Ruby Grade-school - exercism.io spec update - School class updated to match

--- ruby/grade-school/school.rb
+++ ruby/grade-school/school.rb
@@ -1,7 +1,6 @@
 class School
-  def db
-    # deep copy to also clone arrays of students in grades
-    Hash[grade_numbers.map { |grade_number| clone_grade grade_number }]
+  def to_hash
+    Hash[grade_numbers.map { |grade_number| sorted_grade grade_number }]
   end
 
   def add(student_name, grade_number)
@@ -9,11 +8,7 @@ class School
   end
 
   def grade(grade_number)
-    get_grade(grade_number).clone
-  end
-
-  def sort
-    Hash[grade_numbers.map { |grade_number| sort_grade grade_number }]
+    (students[grade_number] || []).sort
   end
 
   private
@@ -26,15 +21,7 @@ class School
     students.keys.sort
   end
 
-  def sort_grade(grade_number)
-    [grade_number, get_grade(grade_number).sort]
-  end
-
-  def clone_grade(grade_number)
+  def sorted_grade(grade_number)
     [grade_number, grade(grade_number)]
-  end
-
-  def get_grade(grade_number)
-    students[grade_number] || []
   end
 end
