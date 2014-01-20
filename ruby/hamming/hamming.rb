c32f04a3b9603c5a525c67b41ee2284b1b55fcb6
Trevor Barnett <mr.ullet@gmail.com>
2014-01-20 09:34:11 +0000

    :construction_worker: Split to two methods to perhaps add a little clarity as to why rejecting b.nil?

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,5 +1,11 @@
 class Hamming
   def self.compute(strandA, strandB)
-    strandA.chars.zip(strandB.chars).reject { |a, b| b.nil? || a == b }.size
+    positional_pairs(strandA, strandB).reject { |a, b| a == b }.size
+  end
+
+  private
+
+  def self.positional_pairs(strandA, strandB)
+    strandA.chars.zip(strandB.chars).reject { |_, b| b.nil? }
   end
 end
