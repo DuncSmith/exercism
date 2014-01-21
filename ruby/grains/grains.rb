c901b927d4a0be2c2dbf8ae9dde2d85d7a9d55c0
Trevor Barnett <mr.ullet@gmail.com>
2014-01-21 11:03:45 +0000

    :construction_worker: Ruby Grains - Over-optimisation using an accumulator to reduce the number of repeated calculations

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,9 +1,26 @@
 class Grains
   def square(number)
-    number == 1 ? 1 : 2 * square(number-1)
+    square_acc(number, 1)
   end
 
   def total
-    (1..64).map {|n| square n }.reduce(:+)
+    sum_acc(64, 1, 1)
+  end
+
+  private
+
+  def square_acc(number, accumulator)
+    number == 1 ? accumulator : square_acc(number-1, 2 * accumulator)
+  end
+
+  def sum_acc(number, accumulator_square, accumulator_sum)
+    if number == 1
+      accumulator_sum
+    else
+      sum_acc(
+        number-1, 
+        2 * accumulator_square, 
+        accumulator_sum + 2 * accumulator_square)
+    end
   end
 end
