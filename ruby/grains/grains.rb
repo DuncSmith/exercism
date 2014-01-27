caa34419c5360f335e441a4da71d8db09472ea42
Trevor Barnett <mr.ullet@gmail.com>
2014-01-27 19:58:58 +0000

    :construction_worker: Ruby Grains - Direct calculation of both Square and Total

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,9 +1,9 @@
 class Grains
   def square(number)
-    number == 1 ? 1 : 2 * square(number-1)
+    2**(number - 1)
   end
 
   def total
-    (1..64).map {|n| square n }.reduce(:+)
+    square(65) - 1
   end
 end
