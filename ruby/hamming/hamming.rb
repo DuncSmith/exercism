78ad0cfb38fc4520aeb22c01b6c60e1940ea9173
Trevor Barnett <mr.ullet@gmail.com>
2014-01-27 19:37:07 +0000

    :construction_worker: Ruby Hamming - Using count rather than reject then size + simply take the correct number of pairs

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,11 +1,11 @@
 class Hamming
-  def self.compute(strandA, strandB)
-    positional_pairs(strandA, strandB).reject { |a, b| a == b }.size
+  def self.compute(strand_a, strand_b)
+    positional_pairs(strand_a, strand_b).count { |a, b| a != b }
   end
 
   private
 
-  def self.positional_pairs(strandA, strandB)
-    strandA.chars.zip(strandB.chars).reject { |_, b| b.nil? }
+  def self.positional_pairs(strand_a, strand_b)
+    strand_a.chars.zip(strand_b.chars).take(strand_b.size)
   end
 end
