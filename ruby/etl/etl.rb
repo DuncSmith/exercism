5b5f3108588a67d684ca6f57dbfc86621dee8783
Trevor Barnett <mr.ullet@gmail.com>
2014-04-06 22:27:59 +0100

    :construction_worker: Ruby ETL - Proper naming case and extracted to instance methods, but is it more expressive?

--- ruby/etl/etl.rb
+++ ruby/etl/etl.rb
@@ -1,8 +1,23 @@
 class ETL
-  def self.transform(scoresToLettersMap)
-    letterScorePairs = scoresToLettersMap.map do |score, letters|
+  def self.transform(score_letter_hash)
+    ETL.new(score_letter_hash).letter_score_hash
+  end
+
+  def initialize(score_letter_hash)
+    @score_letter_hash = score_letter_hash
+  end
+
+  def letter_score_hash
+    letter_score_pairs.reduce(:merge)
+  end
+
+  private
+
+  attr_reader :score_letter_hash
+
+  def letter_score_pairs
+    score_letter_hash.map do |score, letters|
       Hash[letters.map {|letter| [letter.downcase, score]}]
     end
-    letterScorePairs.reduce(:merge)
   end
 end
