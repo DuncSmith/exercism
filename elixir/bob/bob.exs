4599b2ea9c5cd11687ba1e9fc23a8350dcdf6d5a
Trevor Barnett <trevorbarnett@nhs.net>
2015-01-13 11:35:38 +0000

    :baby_bottle: elixir bob - Passes all 13 tests

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,7 +1,30 @@
 defmodule Teenager do
   def hey(input) do
     cond do
-
+      silence?(input) -> "Fine. Be that way!"
+      shouted?(input) -> "Woah, chill out!"
+      question?(input) -> "Sure."
+      true -> "Whatever."
     end
+  end
+  
+  def question?(input) do
+    String.ends_with? input, "?"
+  end
+  
+  def shouted?(input) do
+    uppercase?(input) and alpha_chars?(input)
+  end
+  
+  def uppercase?(input) do
+    input == String.upcase(input)
+  end
+  
+  def alpha_chars?(input) do
+    String.match? input, ~r/[[:alpha:]]/
+  end
+  
+  def silence?(input) do
+    String.strip(input) == ""
   end
 end
