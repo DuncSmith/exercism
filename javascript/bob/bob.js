abecdfab3cd27a96d79b11ddef5fcd2f94384606
Trevor Barnett <trevorbarnett@nhs.net>
2015-01-14 13:51:38 +0000

    :scroll: JavaScript Bob - All tests pass

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -6,9 +6,41 @@
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+  if (isShout(input)) {
+    return 'Woah, chill out!';
+  }
+  if (isQuestion(input)) {
+    return 'Sure.';
+  }
+  if (isSilence(input)) {
+    return 'Fine. Be that way!';
+  }
+  return 'Whatever.';
 };
+  
+function isShout(input) {
+  return isUpperCase(input) && containsLetters(input);
+}
+  
+function isUpperCase(input) {
+  return input === input.toUpperCase();
+}
+  
+function containsLetters(input) {
+  return input.match(/[A-Z]/i);
+}
+  
+function isQuestion(input) {
+  return endsWith(input, '?');
+}
+
+function endsWith(input, ending) {
+  var lastIndex = input.lastIndexOf(ending);
+  return lastIndex !== -1 && lastIndex === (input.length - ending.length);
+}
+  
+function isSilence(input) {
+  return input.trim() === "";
+}
 
 module.exports = Bob;
