a99e314bc607c89a798373abaaf10cb2b93f0c87
Trevor Barnett <mr.ullet@gmail.com>
2015-01-15 17:20:26 +0000

    :bird: F# Sum of Multiples - Passes all tests

--- fsharp/sum-of-multiples/SumOfMultiples.fs
+++ fsharp/sum-of-multiples/SumOfMultiples.fs
@@ -1,4 +1,15 @@
 ﻿module SumOfMultiples
 
-type SumOfMultiples() =
-    member this.To(x) = 0
+type SumOfMultiples(divisors) =
+  new() = SumOfMultiples [3;5]
+
+  member this.To(x) =
+    if x <= 1 then 0
+    else (if this.IsMultipleOfAny(x-1, divisors) then x-1 else 0) + this.To(x-1)
+  
+  member this.IsMultipleOfAny(x, divisors) =
+    not divisors.IsEmpty &&
+    (this.IsMultiple(x, divisors.Head) ||
+     this.IsMultipleOfAny(x, divisors.Tail))
+
+  member this.IsMultiple(x, divisor) = (x % divisor) = 0
