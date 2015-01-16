74d04e9274a719790bdebe82c0ed082479c10663
Trevor Barnett <mr.ullet@gmail.com>
2015-01-16 17:31:45 +0000

    :lemon: C# Leap - DivisibleBy and NotDivisibleBy methods for sake of readability

--- csharp/leap/Year.cs
+++ csharp/leap/Year.cs
@@ -1,10 +1,21 @@
 ﻿namespace Exercism.leap
 {
-  public class Year
+  public static class Year
   {
     public static bool IsLeap(int year)
     {
-      return (year % 400 == 0) || ((year % 100 != 0) && (year % 4 == 0));
+      return year.DivisibleBy(400) || 
+             (year.NotDivisibleBy(100) && year.DivisibleBy(4));
+    }
+
+    private static bool NotDivisibleBy(this int value, int divisor)
+    {
+      return !value.DivisibleBy(divisor);
+    }
+
+    private static bool DivisibleBy(this int value, int divisor)
+    {
+      return value%divisor == 0;
     }
   }
 }
