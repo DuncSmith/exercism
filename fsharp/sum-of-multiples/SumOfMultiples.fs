6ac4914dafde62513a54ce460fc785bbeb71ad0c
Trevor Barnett <mr.ullet@gmail.com>
2015-01-16 14:58:51 +0000

    :bird: F# Sum of Multiples - Proper parameter lists not tuples everywhere!

--- fsharp/sum-of-multiples/SumOfMultiples.fs
+++ fsharp/sum-of-multiples/SumOfMultiples.fs
@@ -3,16 +3,16 @@
 type SumOfMultiples(divisors) =
   new() = SumOfMultiples [3;5]
 
-  member this.To(limit) =
+  member this.To limit =
     let rec toTailRecursive limit sum =
       if limit <= 1 then 
         sum
-      else if this.IsMultipleOfAny(limit-1, divisors) then 
+      else if this.IsMultipleOfAny (limit-1) divisors then 
         toTailRecursive (limit-1) (sum + limit-1)
       else
         toTailRecursive (limit-1) sum
     toTailRecursive limit 0
   
-  member this.IsMultipleOfAny(value, divisors) =
+  member this.IsMultipleOfAny value divisors =
     let isMultiple divisor = ((value % divisor) = 0)
     List.exists isMultiple divisors
