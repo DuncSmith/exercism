43283357d4828bb893ceb5e35349f6003aec0060
Trevor Barnett <mr.ullet@gmail.com>
2015-01-16 14:44:47 +0000

    :bird: F# Sum of Multiples - Simplified IsMultipleOfAny

--- fsharp/sum-of-multiples/SumOfMultiples.fs
+++ fsharp/sum-of-multiples/SumOfMultiples.fs
@@ -3,17 +3,16 @@
 type SumOfMultiples(divisors) =
   new() = SumOfMultiples [3;5]
 
-  member this.To(x) =
-    let rec toAcc x acc =
-      if x <= 1 then acc
-      else if this.IsMultipleOfAny(x-1, divisors) then toAcc (x-1) (acc + x-1)
-      else toAcc (x-1) acc
-    toAcc x 0
+  member this.To(limit) =
+    let rec toTailRecursive limit sum =
+      if limit <= 1 then 
+        sum
+      else if this.IsMultipleOfAny(limit-1, divisors) then 
+        toTailRecursive (limit-1) (sum + limit-1)
+      else
+        toTailRecursive (limit-1) sum
+    toTailRecursive limit 0
   
-  member this.IsMultipleOfAny(x, divisors) =
-    match divisors with
-    | [] -> false
-    | divisor :: others -> this.IsMultiple(x, divisor) ||
-                           this.IsMultipleOfAny(x, others)
-
-  member this.IsMultiple(x, divisor) = ((x % divisor) = 0)
+  member this.IsMultipleOfAny(value, divisors) =
+    let isMultiple divisor = ((value % divisor) = 0)
+    List.exists isMultiple divisors
