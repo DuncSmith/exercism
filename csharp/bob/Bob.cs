903463839ef04585451b4d24b441e2e2e2668582
Trevor Barnett <mr.ullet@gmail.com>
2015-01-19 14:29:20 +0000

    :lemon: C# Bob - All tests pass

---
+++ csharp/bob/Bob.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.RegularExpressions;
+
+public class Bob
+{
+  public string Hey(string phrase)
+  {
+    if (Shouted(phrase)) return "Whoa, chill out!";
+    if (Question(phrase)) return "Sure.";
+    if (Silent(phrase)) return "Fine. Be that way!";
+    return "Whatever.";
+  }
+
+  private static bool Shouted(string phrase)
+  {
+    return IsUppercase(phrase) && ContainsLetters(phrase);
+  }
+
+  private static bool IsUppercase(string phrase)
+  {
+     return phrase.Equals(phrase.ToUpper());
+  }
+
+  private static bool ContainsLetters(string phrase)
+  {
+    // match at least one unicode letter (posix not supported in .NET)
+    return Regex.IsMatch(phrase, @"\p{L}");
+  }
+
+  private static bool Question(string phrase)
+  {
+    return phrase.Trim().EndsWith("?");
+  }
+
+  private static bool Silent(string phrase)
+  {
+    return phrase.Trim().Equals(string.Empty);
+  }
+}
