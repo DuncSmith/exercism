cb551e23f38f9a2c9623f3ff7d00cb6768cd1cff
Trevor Barnett <mr.ullet@gmail.com>
2015-01-30 15:18:59 +0000

    :goat: Haskell Bob - Passes some tests

--- haskell/bob/bob.hs
+++ haskell/bob/bob.hs
@@ -1,4 +1,20 @@
 module Bob (responseFor) where
 
+import Data.List
+import Data.Char
+
 responseFor :: String -> String
-responseFor = undefined
+responseFor said | silence said = "Fine. Be that way!"
+                 | shout said = "Woah, chill out!"
+                 | question said = "Sure."
+                 | otherwise = "Whatever."
+                   
+silence :: String -> Bool
+silence [] = True
+silence said = all (\x -> x == ' ') said
+
+shout :: String -> Bool
+shout said = said == map toUpper said
+
+question :: String -> Bool
+question said = isSuffixOf "?" said
