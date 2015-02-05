149332fccaa677e30bedfb585187c801ff994c95
Trevor Barnett <mr.ullet@gmail.com>
2015-02-05 14:03:45 +0000

    :8ball: clojure bob - passes some tests

--- clojure/bob/bob.clj
+++ clojure/bob/bob.clj
@@ -1,2 +1,14 @@
 (ns bob)
-(def response-for (fn [x] ""))
+
+(declare question? ends-with?)
+
+(defn response-for [s] 
+  (cond
+    (question? s) "Sure." 
+    :else "Whatever."))
+
+(defn- question? [s] 
+  (ends-with? \? s))
+
+(defn- ends-with? [c s] 
+  (= (last (seq s)) c))
