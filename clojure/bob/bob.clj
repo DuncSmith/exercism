f26b83009a69e3aa8a321cdc303c6eaba8aa451e
Trevor Barnett <mr.ullet@gmail.com>
2015-02-06 13:37:12 +0000

    :8ball: clojure bob - 3 namespaces, 1 per concern

--- clojure/bob/bob.clj
+++ clojure/bob/bob.clj
@@ -1,27 +1,39 @@
-(ns bob
+(ns string
   (:require [clojure.string :as string]))
 
-(defn- alphabetic? [s]
+(defn alphabetic? [s]
   (re-find #"\p{Alpha}" s))
 
-(defn- upper-case? [s]
+(defn upper-case? [s]
   (= s (string/upper-case s)))
 
-(defn- ends-with? [c s]
+(defn ends-with? [c s]
   (= (last s) c))
-  
-(defn- silence? [s]
+
+(defn blank? [s]
   (string/blank? s))
-  
-(defn- shout? [s]
-  (and (alphabetic? s) (upper-case? s)))
-  
-(defn- question? [s]
-  (ends-with? \? s))
+
+
+(ns phrase
+  (:require [string]))
+
+(defn silence? [s]
+  (string/blank? s))
+
+(defn shout? [s]
+  (and (string/alphabetic? s) (string/upper-case? s)))
+
+(defn question? [s]
+  (string/ends-with? \? s))
+
+
+(ns bob
+  (:require [clojure.string :as string]
+            [phrase]))
 
 (defn response-for [s]
   (cond
-    (silence? s) "Fine. Be that way!"
-    (shout? s) "Woah, chill out!"
-    (question? s) "Sure."
+    (phrase/silence? s) "Fine. Be that way!"
+    (phrase/shout? s) "Woah, chill out!"
+    (phrase/question? s) "Sure."
     :else "Whatever."))
