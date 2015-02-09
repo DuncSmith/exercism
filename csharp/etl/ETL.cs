475b7924adf88255b6cbbde0c54389abab7fc784
Trevor Barnett <mr.ullet@gmail.com>
2015-02-09 14:06:23 +0000

    :lemon: C# ETL - Passes tests

---
+++ csharp/etl/ETL.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class ETL
+{
+  public static
+    Dictionary<string, int> Transform(Dictionary<int, IList<string>> old)
+  {
+    return LetterValuePairs(old)
+      .ToDictionary(lvp => lvp.Key, lvp => lvp.Value);
+  }
+
+  private static IEnumerable<KeyValuePair<string, int>> LetterValuePairs(
+    Dictionary<int, IList<string>> old)
+  {
+    return old.SelectMany(
+      kvp => kvp.Value.Select(
+        letter => new KeyValuePair<string, int>(letter.ToLower(), kvp.Key)));
+  }
+}
