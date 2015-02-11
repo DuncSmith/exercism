90b9246a70be53e2a30236cb576afc93f6723f4b
Trevor Barnett <mr.ullet@gmail.com>
2015-02-11 13:36:53 +0000

    :lemon: C# Hamming - Another one liner using Linq and Tuple

--- csharp/hamming/Hamming.cs
+++ csharp/hamming/Hamming.cs
@@ -1,9 +1,10 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 
 public class Hamming
 {
   public static int Compute(string first, string second)
   {
-    return first.Zip(second, (f, s) => new {f, s}).Count(x => x.f != x.s);
+    return first.Zip(second, Tuple.Create).Count(x => x.Item1 != x.Item2);
   }
 }
