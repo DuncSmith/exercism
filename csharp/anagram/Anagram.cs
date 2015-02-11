bbdd8d21f974f7d6128b8f0156e6fccbb663481d
Trevor Barnett <mr.ullet@gmail.com>
2015-02-11 13:12:43 +0000

    :lemon: C# Anagram - Passes tests & short methods

---
+++ csharp/anagram/Anagram.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+public class Anagram
+{
+  private readonly string _word;
+  private readonly string _lettersInWord;
+
+  public Anagram(string word)
+  {
+    _word = word.ToLower();
+    _lettersInWord = LettersInWord(word);
+  }
+
+  public string[] Match(string[] words)
+  {
+    return words.Where(DifferentWord).Where(SameLetters).ToArray();
+  }
+
+  private bool DifferentWord(string word)
+  {
+    return !_word.Equals(word, StringComparison.InvariantCultureIgnoreCase);
+  }
+
+  private bool SameLetters(string word)
+  {
+    return _lettersInWord.Equals(LettersInWord(word));
+  }
+
+  private static string LettersInWord(string word)
+  {
+    return new string(word.ToLower().OrderBy(c => c).ToArray());
+  }
+}
