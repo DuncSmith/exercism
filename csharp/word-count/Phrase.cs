5740c09f4dcce97dc86de9047e10cd9485f2ec60
Trevor Barnett <mr.ullet@gmail.com>
2015-02-11 21:45:23 +0000

    :lemon: C# Word Count - Regex based, pass all but apostrophes as quotes

---
+++ csharp/word-count/Phrase.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public class Phrase
+{
+  private readonly string _words;
+
+  public Phrase(string words)
+  {
+    _words = words;
+  }
+
+  public Dictionary<string, int> WordCount()
+  {
+    return Regex.Matches(_words, @"(?:\w[\w']*|[\w']*\w)").Cast<Match>()
+      .SelectMany(m => m.Groups.Cast<Group>())
+      .SelectMany(g => g.Captures.Cast<Capture>())
+      .Select(c => c.Value.ToLower())
+      .GroupBy(word => word)
+      .ToDictionary(group => group.Key, group => group.Count());
+  }
+}
