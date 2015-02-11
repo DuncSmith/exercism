e29940e0b1f96fb4b56da12bf96c1261286443f3
Trevor Barnett <mr.ullet@gmail.com>
2015-02-11 13:30:59 +0000

    :lemon: C# Hamming - Passes tests. One line using Linq.

---
+++ csharp/hamming/Hamming.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+public class Hamming
+{
+  public static int Compute(string first, string second)
+  {
+    return first.Zip(second, (f, s) => f == s ? 0 : 1).Sum(x => x);
+  }
+}
