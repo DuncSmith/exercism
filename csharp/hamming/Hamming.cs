97293b1bf4631a4d06ed31bcadc96d2c58e19504
Trevor Barnett <mr.ullet@gmail.com>
2015-02-11 13:43:35 +0000

    :lemon: C# Hamming - Split into functions for readability

--- csharp/hamming/Hamming.cs
+++ csharp/hamming/Hamming.cs
@@ -1,10 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 public class Hamming
 {
   public static int Compute(string first, string second)
   {
-    return first.Zip(second, Tuple.Create).Count(x => x.Item1 != x.Item2);
+    return PositionalPairs(first, second).Count(Mismatched);
+  }
+
+  private static IEnumerable<Tuple<char, char>> PositionalPairs(
+    string first, string second)
+  {
+    return first.Zip(second, Tuple.Create);
+  }
+
+  private static bool Mismatched(Tuple<char, char> pair)
+  {
+    return pair.Item1 != pair.Item2;
   }
 }
