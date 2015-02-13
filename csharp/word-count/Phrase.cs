1cd9f75608660c25e1158d2df27ef162320236ca
Trevor Barnett <mr.ullet@gmail.com>
2015-02-13 13:46:49 +0000

    :lemon: C# Word Count - Passes tests using 'poor-man' pattern matching

--- csharp/word-count/Phrase.cs
+++ csharp/word-count/Phrase.cs
@@ -1,13 +1,30 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public class Phrase
 {
+  private readonly string _text;
+
   public Phrase(string text)
   {
+    _text = text;
   }
 
   public Dictionary<string, int> WordCount()
   {
-    return null;
+    var wordChars = Enumerable.Range('a', 26)
+      .Union(Enumerable.Range('A', 26))
+      .Union(Enumerable.Range('0', 10))
+      .Select(i => (char) i)
+      .Concat(new[] {'\''});
+    var allChars = Enumerable.Range('\0', 256).Select(i => (char) i);
+    var nonWordChars = allChars.Except(wordChars);
+    return _text
+      .Split(nonWordChars.ToArray(), StringSplitOptions.RemoveEmptyEntries)
+      .Select(w => w.Trim('\''))
+      .Where(w => w != string.Empty)
+      .GroupBy(w => w.ToLower())
+      .ToDictionary(g => g.Key, g => g.Count());
   }
 }
