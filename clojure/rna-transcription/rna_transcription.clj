e9c1431d5a58f5ef905aef498b39ca4fb39bc0b0
Trevor Barnett <mr.ullet@gmail.com>
2015-02-18 13:41:26 +0000

    :8ball: clojure RNA Transcription - case better than cond here

--- clojure/rna-transcription/rna_transcription.clj
+++ clojure/rna-transcription/rna_transcription.clj
@@ -2,10 +2,10 @@
 
 (defn- single-to-rna [c]
   (assert (some #{c} [\A \C \G \T]) (str "Unknown nucleotide: " c))
-  (cond 
-    (= c \A) \U
-    (= c \C) \G
-    (= c \G) \C
-    (= c \T) \A))
+  (case c
+    \A \U
+    \C \G
+    \G \C
+    \T \A))
 
 (defn to-rna [s] (apply str (map single-to-rna s)))
