f1d690b1a3337e7768a288e5e418400084dd3d83
Trevor Barnett <mr.ullet@gmail.com>
2015-02-18 13:38:54 +0000

    :8ball: clojure RNA Transcription - passes tests

--- clojure/rna-transcription/rna_transcription.clj
+++ clojure/rna-transcription/rna_transcription.clj
@@ -1,3 +1,11 @@
 (ns rna-transcription)
 
-(defn to-rna [s] s)
+(defn- single-to-rna [c]
+  (assert (some #{c} [\A \C \G \T]) (str "Unknown nucleotide: " c))
+  (cond 
+    (= c \A) \U
+    (= c \C) \G
+    (= c \G) \C
+    (= c \T) \A))
+
+(defn to-rna [s] (apply str (map single-to-rna s)))
