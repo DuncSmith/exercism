0c3eeac40986963399651e3ac40e23cde641f546
Trevor Barnett <mr.ullet@gmail.com>
2015-02-19 13:02:05 +0000

    :sheep: Fixed broken F# project

--- csharp/leap/Year.cs
+++ csharp/leap/Year.cs
@@ -1,21 +1,18 @@
-﻿namespace Exercism.leap
+﻿public static class Year
 {
-  public static class Year
+  public static bool IsLeap(int year)
   {
-    public static bool IsLeap(int year)
-    {
-      return year.DivisibleBy(400) || 
-             (year.NotDivisibleBy(100) && year.DivisibleBy(4));
-    }
+    return year.DivisibleBy(400) ||
+           (year.NotDivisibleBy(100) && year.DivisibleBy(4));
+  }
 
-    private static bool NotDivisibleBy(this int value, int divisor)
-    {
-      return !value.DivisibleBy(divisor);
-    }
+  private static bool NotDivisibleBy(this int value, int divisor)
+  {
+    return !value.DivisibleBy(divisor);
+  }
 
-    private static bool DivisibleBy(this int value, int divisor)
-    {
-      return value%divisor == 0;
-    }
+  private static bool DivisibleBy(this int value, int divisor)
+  {
+    return value%divisor == 0;
   }
 }
