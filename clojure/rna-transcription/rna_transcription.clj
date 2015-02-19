62ad08e2876ea9db2b0ccbae0dee50392a84be56
Trevor Barnett <mr.ullet@gmail.com>
2015-02-19 09:43:32 +0000

    :8ball: clojure RNA Transcription - map hashmap directly

--- clojure/rna-transcription/rna_transcription.clj
+++ clojure/rna-transcription/rna_transcription.clj
@@ -1,8 +1,6 @@
 (ns rna-transcription)
 
-(defn- single-to-rna [c]
+(defn to-rna [s] 
   (def rna-map {\A \U, \C \G, \G \C, \T \A})
-  (assert (contains? rna-map c) (str "Unknown nucleotide: " c))
-  (get rna-map c))
-
-(defn to-rna [s] (apply str (map single-to-rna s)))
+  (assert (every? (partial contains? rna-map) s) (str "Invalid RNA strand: " s))
+  (apply str (map rna-map s)))
