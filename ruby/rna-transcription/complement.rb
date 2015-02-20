25a134037c734e7e68a022b6d7fadf8f67848d70
Trevor Barnett <mr.ullet@gmail.com>
2015-02-20 14:09:40 +0000

    :construction_worker: Ruby RNA Transcription - tr method

--- ruby/rna-transcription/complement.rb
+++ ruby/rna-transcription/complement.rb
@@ -1,29 +1,13 @@
 class Complement
+  Dna_nucleotides = 'CGAT'
+  Rna_nucleotides = 'GCUA'
+  private_constant :Dna_nucleotides, :Rna_nucleotides
+  
   def self.of_dna(strand)
-    translate_strand(dna_rna_map, strand)
+    strand.tr(Dna_nucleotides, Rna_nucleotides)
   end
 
   def self.of_rna(strand)
-    translate_strand(rna_dna_map, strand)
-  end
-
-  class << self
-    private
-
-    def translate_strand(map, strand)
-      translate_array(map, strand.split('')).join('')
-    end
-    
-    def translate_array(map, nucleotides)
-      map.values_at(*nucleotides)
-    end
-  
-    def dna_rna_map
-      {'C' => 'G', 'G' => 'C', 'A' => 'U', 'T' => 'A'}
-    end
-
-    def rna_dna_map
-      dna_rna_map.invert
-    end
+    strand.tr(Rna_nucleotides, Dna_nucleotides)
   end
 end
