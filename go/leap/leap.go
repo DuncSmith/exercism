3c4a5095fdfd4141709eba30e9e8c3b1280d97be
Trevor Barnett <mr.ullet@gmail.com>
2015-02-27 18:51:15 +0000

    :mag: Go Leap - Passes with helper func

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -3,5 +3,9 @@ package leap
 const TestVersion = 1
 
 func IsLeapYear(year int) bool {
-	return false
+	return (multiple(4, year) && !multiple(100, year)) || multiple(400, year)
+}
+
+func multiple(divisor int, year int) bool {
+	return year%divisor == 0
 }
