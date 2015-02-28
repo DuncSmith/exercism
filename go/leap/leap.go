3ed748656cf18116ff9ceaf63d9563a0e6889838
Trevor Barnett <mr.ullet@gmail.com>
2015-02-28 11:23:27 +0000

    :mag: Go Leap - Wrap longish line

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -3,7 +3,8 @@ package leap
 const TestVersion = 1
 
 func IsLeapYear(year int) bool {
-	return multiple(4, year) && (!multiple(100, year) || multiple(400, year))
+	return multiple(4, year) &&
+		(!multiple(100, year) || multiple(400, year))
 }
 
 func multiple(divisor int, year int) bool {
