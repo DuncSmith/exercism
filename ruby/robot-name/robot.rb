69f7c5ab4f52c4eccf19a7e685186bddee187137
Trevor Barnett <mr.ullet@gmail.com>
2015-03-07 08:34:38 +0000

    :construction_worker: Robot Name - robot registry

--- ruby/robot-name/robot.rb
+++ ruby/robot-name/robot.rb
@@ -1,40 +1,73 @@
 class Robot
-  @names = {}
-  class << self; attr_reader :names end
+  def initialize
+    RobotRegistry.register(self)
+  end
 
   def name
-    @name ||= generate_unique_name
+    RobotRegistry.robot_name(self)
   end
 
   def reset
-    @name = nil
+    RobotRegistry.reregister(self)
+  end
+end
+
+class RobotRegistry
+  def self.reregister(robot)
+    unregister(robot)
+    register(robot)
   end
 
-  private
-
-  def generate_unique_name
-    name = ''
-    loop { break unless Robot.names.values.include?(name = generate_name) }
-    Robot.names[self] = name
-    name
+  def self.unregister(robot)
+    names.delete(robot)
   end
 
-  def generate_name
+  def self.register(robot)
+    names[robot] = unique_name
+  end
+
+  def self.robot_name(robot)
+    names[robot]
+  end
+
+  class << self
+    private
+
+    def unique_name
+      name = ''
+      loop do
+        break unless names.values.include?(name = RobotName.generate)
+      end
+      name
+    end
+
+    def names
+      @names ||= {}
+    end
+  end
+end
+
+class RobotName
+  def self.generate
     name = ''
     2.times { name << rand_letter }
     3.times { name << rand_digit }
     name
   end
 
-  def rand_letter
-    rand_from_range(('A'..'Z'))
-  end
+  class << self
+    private
 
-  def rand_digit
-    rand_from_range(('0'..'9'))
-  end
+    def rand_letter
+      rand_from_range(('A'..'Z'))
+    end
 
-  def rand_from_range(range)
-    range.to_a[rand(range.count)]
+    def rand_digit
+      rand_from_range(('0'..'9'))
+    end
+
+    def rand_from_range(range)
+      range.to_a[rand(range.count)]
+    end
   end
 end
