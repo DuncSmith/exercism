83932a43afad47cb0201c8339801115cb8e56a17
Trevor Barnett <mr.ullet@gmail.com>
2015-03-16 12:13:25 +0000

    :lemon: C# Grade School - Internal SortedList

---
+++ csharp/grade-school/School.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class School
+{
+  private readonly List<SortedList<string, string>> _roster =
+    new List<SortedList<string, string>>();
+
+  public IList<List<string>> Roster
+  {
+    get
+    {
+      return _roster.Select(GradeAsList).ToList();
+    }
+  }
+
+  public void Add(string name, int grade)
+  {
+    EnsureGradeInitialized(grade);
+    AddNameToGrade(name, grade);
+  }
+
+  public List<string> Grade(int grade)
+  {
+    return _roster.Count <= grade
+      ? new List<string>()
+      : GradeAsList(_roster[grade]);
+  }
+
+  private static List<string> GradeAsList(SortedList<string, string> grade)
+  {
+    return grade.Values.ToList();
+  }
+
+  private void EnsureGradeInitialized(int grade)
+  {
+    while (_roster.Count <= grade)
+      _roster.Add(new SortedList<string, string>());
+  }
+
+  private void AddNameToGrade(string name, int grade)
+  {
+    _roster[grade].Add(name, name);
+  }
+}
