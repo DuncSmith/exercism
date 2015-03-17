1dbf6cffb00cdd814ead16ff7dac9c5818196883
Trevor Barnett <mr.ullet@gmail.com>
2015-03-17 16:31:21 +0000

    :bird: F# Bob - Cleaner partial active patterns

--- fsharp/bob/Bob.fs
+++ fsharp/bob/Bob.fs
@@ -6,20 +6,18 @@ type Bob(greeting : string) =
 
   let lowercase(str:string) = str.ToLower().Equals(str)
 
-  let (|Shout|_|) (str : string) = 
-    if uppercase(str) && not(lowercase(str)) 
-    then Some("Whoa, chill out!")
-    else None
+  let (|Shout|_|) (str : string) =
+    if uppercase(str) && not(lowercase(str)) then Some() else None
 
-  let (|Question|_|) (str : string) = 
-    if str.EndsWith("?") then Some("Sure.") else None
+  let (|Question|_|) (str : string) =
+    if str.EndsWith("?") then Some() else None
 
-  let (|Silence|_|) (str : string) = 
-    if str.Trim().Equals("") then Some("Fine. Be that way!") else None
+  let (|Silence|_|) (str : string) =
+    if str.Trim().Equals("") then Some() else None
 
   member this.hey() =
     match greeting with
-      | Shout response -> response
-      | Question response -> response
-      | Silence response -> response
+      | Shout -> "Whoa, chill out!"
+      | Question -> "Sure."
+      | Silence -> "Fine. Be that way!"
       | _ -> "Whatever."
