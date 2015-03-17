46b6df3d790add4ec27de5888d02010f93354b7a
Trevor Barnett <mr.ullet@gmail.com>
2015-03-17 13:12:14 +0000

    :bird: F# Bob - passes tests C# style

---
+++ fsharp/bob/Bob.fs
@@ -0,0 +1,20 @@
+﻿module Bob
+
+type Bob(greeting : string) =
+
+  member this.hey() = 
+    if this.shouted() then "Whoa, chill out!"
+    else if this.question() then "Sure."
+    else if this.silence() then "Fine. Be that way!"
+    else "Whatever."
+
+  member private this.shouted() = 
+    this.uppercase() && not(this.lowercase())
+
+  member private this.uppercase() = greeting.ToUpper().Equals(greeting)
+
+  member private this.lowercase() = greeting.ToLower().Equals(greeting)
+
+  member private this.question() = greeting.EndsWith("?")
+
+  member private this.silence() = greeting.Trim().Equals("")
