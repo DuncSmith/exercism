bd68381ff9e4bba110ebe3703f62818509eae1f1
Trevor Barnett <mr.ullet@gmail.com>
2015-03-17 13:43:57 +0000

    :bike: perl5 bob - restored lost Bob with update for new tests

--- perl5/bob/Bob.pm
+++ perl5/bob/Bob.pm
@@ -1,24 +1,48 @@
-#
-# This is a SKELETON file and has been provided to enable you to get working on the
-# first exercise more quickly.
-#
-
-use 5.006;
-use strict;
-use warnings;
-
 package Bob;
 
-our $VERSION = '1.000';
+use v5.18; # required for 'lexical_subs'
+use strict;
+no warnings "experimental::lexical_subs";
+use feature 'lexical_subs';
 
-use Exporter 5.57 qw(import);
+my sub is_shouted; my sub is_question; my sub is_silence;
+my sub contains_alpha; my sub is_upper_case;
 
-our @EXPORT_OK = qw(hey);
+sub hey 
+{ 
+    my $message = shift;
 
-sub hey {
-#
-# YOUR CODE GOES HERE
-#
+    return 'Fine. Be that way!' if is_silence($message);
+    return 'Whoa, chill out!' if is_shouted($message);
+    return 'Sure.' if is_question($message);
+    return 'Whatever.';
+}
+
+sub is_shouted 
+{ 
+    my $message = shift;
+
+    is_words($message) && is_upper_case($message);
+}
+
+sub is_question
+{
+    shift =~ /\?$/;
+}
+
+sub is_silence
+{
+    shift =~ /^\s*$/;
+}
+
+sub is_words
+{
+    shift =~ /\p{Letter}/;
+}
+
+sub is_upper_case
+{
+    shift !~ /\p{Lowercase}/;
 }
 
 1;
