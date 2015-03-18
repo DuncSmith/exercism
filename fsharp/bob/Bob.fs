32614398a1d7e7bae4d3ee5e8210edb58b32c894
Trevor Barnett <mr.ullet@gmail.com>
2015-03-18 16:18:45 +0000

    :bird: F# Bob - nested helper functions

--- fsharp/bob/Bob.fs
+++ fsharp/bob/Bob.fs
@@ -1,22 +1,15 @@
 ﻿module Bob
 
 type Bob(greeting : string) =
-  let (-&-) (first : 'a -> bool) (second : 'a -> bool) =
-    fun x -> first x && second x
-
-  let contains x seq = seq |> Seq.exists ((=) x)
-
-  let uppercase c = {'A'..'Z'} |> contains c
-
-  let lowercase c = {'a'..'z'} |> contains c
-
-  let someUppercase seq = seq |> Seq.exists uppercase
-
-  let noneLowercase seq = seq |> Seq.forall (not << lowercase)
-
   let (|Is|_|) predicate x = if predicate x then Some() else None
 
-  let shout(str : string) = str |> (someUppercase -&- noneLowercase)
+  let shout(str : string) = 
+    let contains x seq = seq |> Seq.exists ((=) x)
+    let uppercase c = {'A'..'Z'} |> contains c
+    let lowercase c = {'a'..'z'} |> contains c
+    let someUppercase seq = seq |> Seq.exists uppercase
+    let noneLowercase seq = seq |> Seq.forall (not << lowercase)
+    someUppercase str && noneLowercase str
 
   let question(str : string) = str.EndsWith "?"
 
