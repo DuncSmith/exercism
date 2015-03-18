344e943a5b507c36c06613a5ecc310cf1eeb51e4
Trevor Barnett <mr.ullet@gmail.com>
2015-03-18 15:40:10 +0000

    :bird: F# Bob - less parentheses and alpha not 'is lower'

--- fsharp/bob/Bob.fs
+++ fsharp/bob/Bob.fs
@@ -2,17 +2,19 @@
 
 type Bob(greeting : string) =
 
-  let (|Is|_|) predicate x = if predicate(x) then Some() else None
-
   let uppercase(str : string) = str.ToUpper().Equals(str)
 
-  let lowercase(str : string) = str.ToLower().Equals(str)
+  let alpha(str : string) =
+    let alphas = ['a'..'z'] @ ['A'..'Z']
+    str |> Seq.exists (fun c -> alphas |> Seq.exists ((=) c))
 
-  let shout (str : string) = uppercase(str) && not(lowercase(str))
+  let (|Is|_|) predicate x = if predicate x then Some() else None
 
-  let question (str : string) = str.EndsWith("?")
+  let shout(str : string) = uppercase str && alpha str
 
-  let silence (str : string) = str.Trim().Equals("")
+  let question(str : string) = str.EndsWith "?"
+
+  let silence(str : string) = str.Trim().Equals ""
 
   member this.hey() =
     match greeting with
