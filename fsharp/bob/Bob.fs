504abe01d09a896334b34656c5624b0068b23514
Trevor Barnett <mr.ullet@gmail.com>
2015-03-19 20:28:41 +0000

    :bird: F# Bob - a few tweaks to be a bit more F# style

--- fsharp/bob/Bob.fs
+++ fsharp/bob/Bob.fs
@@ -4,17 +4,17 @@ type Bob(greeting : string) =
 
   member this.hey() = 
     if this.shouted() then "Whoa, chill out!"
-    else if this.question() then "Sure."
-    else if this.silence() then "Fine. Be that way!"
+    elif this.question() then "Sure."
+    elif this.silence() then "Fine. Be that way!"
     else "Whatever."
 
   member private this.shouted() = 
     this.uppercase() && not(this.lowercase())
 
-  member private this.uppercase() = greeting.ToUpper().Equals(greeting)
+  member private this.uppercase() = greeting.ToUpper() = greeting
 
-  member private this.lowercase() = greeting.ToLower().Equals(greeting)
+  member private this.lowercase() = greeting.ToLower() = greeting
 
-  member private this.question() = greeting.EndsWith("?")
+  member private this.question() = greeting.EndsWith "?"
 
-  member private this.silence() = greeting.Trim().Equals("")
+  member private this.silence() = greeting.Trim() = ""
