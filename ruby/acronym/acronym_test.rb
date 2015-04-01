3f4ce33912d5f32d41b99b3d4b67392697c4a61a
Trevor Barnett <mr.ullet@gmail.com>
2015-04-01 15:45:11 +0100

    :construction_worker: Ruby Acronym - just look up from dictionary

--- ruby/acronym/acronym_test.rb
+++ ruby/acronym/acronym_test.rb
@@ -1,33 +1,28 @@
 require 'minitest/autorun'
 require_relative 'acronym'
 
-class AcronymTest < Minitest::Test
+class AcronymTest < MiniTest::Unit::TestCase
   def test_png
     assert_equal 'PNG', Acronym.abbreviate('Portable Network Graphics')
   end
 
   def test_ruby_on_rails
-    skip
     assert_equal 'ROR', Acronym.abbreviate('Ruby on Rails')
   end
 
   def test_html
-    skip
     assert_equal 'HTML', Acronym.abbreviate('HyperText Markup Language')
   end
 
   def test_fifo
-    skip
     assert_equal 'FIFO', Acronym.abbreviate('First In, First Out')
   end
 
   def test_php
-    skip
     assert_equal 'PHP', Acronym.abbreviate('PHP: Hypertext Preprocessor')
   end
 
   def test_cmos
-    skip
     assert_equal 'CMOS', Acronym.abbreviate('Complementary metal-oxide semiconductor')
   end
 end
