c45ef796aad166908506b94597e481fc3232564b
Trevor Barnett <mr.ullet@gmail.com>
2015-06-03 17:49:57 +0100

    :bird: F# Sum of Multiples - re-enabled all tests

--- fsharp/sum-of-multiples/SumOfMultiples.fs
+++ fsharp/sum-of-multiples/SumOfMultiples.fs
@@ -6,7 +6,7 @@ type SumOfMultiples(divisors) =
   member this.To limit =
     let values = [1 .. (limit-1)]
     List.sum (List.choose this.IsMultipleOfAny values)
-  
+
   member private this.IsMultipleOfAny value =
     let isMultiple divisor = ((value % divisor) = 0)
     if (List.exists isMultiple divisors) then Some(value) else None
