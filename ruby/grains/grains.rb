0cf19ae8a36fb4d4074beaca549b5677d74ddb84
Trevor Barnett <mr.ullet@gmail.com>
2015-06-03 10:41:13 +0100

    :construction_worker: Ruby Grains - only self methods

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,21 +1,11 @@
 class Grains
+  @squares = [0, 1]
+
   def self.square(number)
-    Grains.new.square(number)
-  end
-
-  def self.total
-    Grains.new.total
-  end
-
-  def initialize
-    @squares = [0, 1]
-  end
-
-  def square(number)
     @squares[number] ||= 2 * square(number-1)
   end
 
-  def total
-    (1..64).map {|n| square n }.reduce(:+)
+  def self.total
+    (1..64).map(&method(:square)).reduce(:+)
   end
 end
