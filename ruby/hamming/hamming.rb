2fbf9c46b53d133c87f26393c9a09607f4db7b2e
Trevor Barnett <mr.ullet@gmail.com>
2015-06-03 11:42:52 +0100

    :construction_worker: Ruby Hamming - pass new version of tests

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,4 +1,6 @@
 class Hamming
+  VERSION = 1
+  
   def self.compute(strand, other)
     Hamming.new(strand, other).distance
   end
@@ -6,6 +8,7 @@ class Hamming
   def initialize(strand, other)
     @strand = strand
     @other = other
+    validate
   end
 
   def distance
@@ -15,6 +18,10 @@ class Hamming
   private
 
   attr_reader :strand, :other
+
+  def validate
+    throw "Strand lengths differ" if strand.size != other.size
+  end
 
   def positional_pairs
     strand.chars.zip(other.chars).take(other.size)
