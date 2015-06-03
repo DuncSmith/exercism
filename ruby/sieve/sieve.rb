21108535325312f6b2909ca93c33e35e9fd2e747
Trevor Barnett <mr.ullet@gmail.com>
2015-06-03 20:31:28 +0100

    :construction_worker: Ruby Sieve - alternate working sieve

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -2,26 +2,36 @@ class Sieve
   def initialize(limit)
     @limit = limit
   end
-  
+
   def primes
     @primes || find_primes
   end
-  
+
   private
-  
+
   attr_reader :limit
-  
+
   def find_primes
-    numbers = (2..limit).map { |n| [n, false] }
-    prime = 2
-    while prime
-      remainder = numbers.drop(prime - 1)
-      remainder.each do |n|
-        next if n[1]
-        n[1] = true if (n[0] % prime) == 0
+    remainder = numbers = (2..limit).to_a
+    while remainder.any?
+      prime = remainder.first
+      remainder.drop(1).each do |n|
+        mark(numbers, n) if multiple?(prime, n)
       end
-      prime = (remainder.find { |n, _| n > 0 } || [])[0]
+      remainder = numbers.drop(prime - 1).reject(&method(:marked?))
     end
-    numbers.reject { |_, marked| marked }.map { |n, _| n }
+    numbers.reject(&method(:marked?))
+  end
+
+  def mark(numbers, number)
+    numbers[number - 2] = 0
+  end
+
+  def marked?(number)
+    number == 0
+  end
+
+  def multiple?(divisor, number)
+    (number % divisor) == 0
   end
 end
