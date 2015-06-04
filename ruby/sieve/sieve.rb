e726e436b0d2a57553bdb00b47a51dbc5b7756f8
Trevor Barnett <mr.ullet@gmail.com>
2015-06-04 12:43:41 +0100

    :construction_worker: Ruby Sieve - proper Sieve of Eratosthenes

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -12,14 +12,24 @@ class Sieve
   attr_reader :limit
 
   def find_primes
-    possible_primes = (2..limit)
-    primes, possible_primes =
-      sieve(primes || [], possible_primes) while possible_primes.any?
+    numbers = (2..limit).to_a
+    while (multiple = prime = next_prime(numbers, prime))
+      (primes ||= []) << prime
+      mark(numbers, multiple) while (multiple += prime) <= limit
+    end
     primes
   end
 
-  def sieve(primes, possible_primes)
-    prime, *possible = *possible_primes
-    [primes + [prime], possible.reject { |n| n % prime == 0 }]
+  def next_prime(numbers, prime)
+    return numbers[0] unless prime
+    numbers.drop(prime - 1).find(&method(:unmarked?))
+  end
+
+  def mark(numbers, number)
+    numbers[number - 2] = 0
+  end
+
+  def unmarked?(number)
+    number > 0
   end
 end
