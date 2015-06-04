d3e9ee0dddfbf5455eb8d8b6dfd4a11d36775e92
Trevor Barnett <mr.ullet@gmail.com>
2015-06-04 10:25:01 +0100

    :construction_worker: Ruby Sieve - simplify next prime expression

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -14,9 +14,8 @@ class Sieve
   def sieve(numbers, prime)
     return numbers unless prime
     known_primes = numbers.take_while { |n| n <= prime }
-    candidate_primes = numbers.drop_while { |n| n <= prime }
-    sieve(
-      known_primes + candidate_primes.reject { |n| n % prime == 0 },
-      numbers.find { |n| n > prime })
+    possible_primes = numbers.drop_while { |n| n <= prime }
+                      .reject { |n| n % prime == 0 }
+    sieve(known_primes + possible_primes, possible_primes.first)
   end
 end
