d6e411244ab5d6ef09dbbb443993b8614db2bc77
Trevor Barnett <mr.ullet@gmail.com>
2015-06-05 20:07:26 +0100

    :construction_worker: Ruby Prime Factor - inlined: faster, simpler, more readable!

--- ruby/prime-factors/prime_factors.rb
+++ ruby/prime-factors/prime_factors.rb
@@ -1,45 +1,15 @@
-require 'benchmark'
 require 'prime'
 
-module EnumerableExtensions; end
-
 module PrimeFactors
-  E = EnumerableExtensions
-
   def self.for(number)
-    found_all_factors = ->(_, prime) { number < prime }
-    primes = Prime.each(number).extend(E)
-    primes.reduce_until(found_all_factors, []) do |a, prime|
-      divisible = -> { number % prime == 0 }
-      factor = -> { (number /= prime) && prime }
-      a + E.generate_while(divisible, &factor)
+    factors = []
+    Prime.each(number) do |prime|
+      break if number < prime
+      while number % prime == 0
+        number /= prime
+        factors << prime
+      end
     end
-  end
-end
-
-module EnumerableExtensions
-  E = EnumerableExtensions
-
-  def reduce_until(predicate, a = nil)
-    E.reduce_until(predicate, self, a, &Proc.new)
-  end
-
-  def self.generate_while(predicate)
-    return enum_for(:generate_while, predicate) unless block_given?
-    a = []
-    loop do
-      break unless predicate.call
-      a << yield
-    end
-    a
-  end
-
-  def self.reduce_until(predicate, list, a = nil)
-    return enum_for(:reduce_until, args) unless block_given?
-    list.each_with_index do |e, i|
-      break if predicate.call(a, e)
-      a = (i > 0 || a) ? yield(a, e) : e
-    end
-    a
+    factors
   end
 end
