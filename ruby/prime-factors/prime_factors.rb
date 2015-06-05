8b30206258808912129171d07e7fdc57e81a2a2e
Trevor Barnett <mr.ullet@gmail.com>
2015-06-05 20:45:48 +0100

    :construction_worker: Ruby Prime Factor - check sqrt to stop asap

--- ruby/prime-factors/prime_factors.rb
+++ ruby/prime-factors/prime_factors.rb
@@ -4,12 +4,20 @@ module PrimeFactors
   def self.for(number)
     factors = []
     Prime.each(number) do |prime|
-      break if number < prime
-      while number % prime == 0
-        number /= prime
-        factors << prime
-      end
+      break if number == 1
+      factors << number && break if Math.sqrt(number) < prime
+      (factors_for_prime, number) = factor_by_prime(number, prime)
+      factors += factors_for_prime
     end
     factors
+  end
+
+  def self.factor_by_prime(number, prime)
+    factors = []
+    while number % prime == 0
+      number /= prime
+      factors << prime
+    end
+    [factors, number]
   end
 end
