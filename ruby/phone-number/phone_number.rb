a97ec763c233aadf8eb03576f4396a621916ab77
Trevor Barnett <mr.ullet@gmail.com>
2015-06-05 10:59:08 +0100

    :construction_worker: Ruby Phone Number - positive starts for slices

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -12,15 +12,15 @@ class PhoneNumber
   end
 
   def area_code
-    clean_number[-10, 3]
+    clean_number[0, 3]
   end
 
   def prefix
-    clean_number[-7, 3]
+    clean_number[3, 3]
   end
 
   def line
-    clean_number[-4, 4]
+    clean_number[6, 4]
   end
 
   private
@@ -28,11 +28,23 @@ class PhoneNumber
   attr_reader :unclean_number
 
   def clean_number
-    @clean_number ||= valid_number? ? stripped_number : invalid_number
+    @clean_number ||= valid_number? ? national_number : invalid_number
   end
 
   def valid_number?
     only_digits? && (national_us_number? || international_us_number?)
+  end
+
+  def national_number
+    stripped_number[-10, 10]
+  end
+
+  def invalid_number
+    '0000000000'
+  end
+
+  def only_digits?
+    stripped_number.chars.all? { |c| ('0'..'9').include?(c) }
   end
 
   def national_us_number?
@@ -43,16 +55,8 @@ class PhoneNumber
     stripped_number.size == 11 && stripped_number[0] == '1'
   end
 
-  def only_digits?
-    stripped_number.chars.all? { |c| ('0'..'9').include?(c) }
-  end
-
   def stripped_number
     keep = ('0'..'9').to_a + ('a'..'z').to_a
     unclean_number.downcase.chars.keep_if { |c| keep.include?(c) }.join
-  end
-
-  def invalid_number
-    '0000000000'
   end
 end
