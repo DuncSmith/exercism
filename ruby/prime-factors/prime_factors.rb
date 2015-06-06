2126d12afe44bb97e6d147bbbd5ea07b6c259288
Trevor Barnett <mr.ullet@gmail.com>
2015-06-06 12:35:51 +0100

    :construction_worker: Ruby Prime Factors - without using Prime

--- ruby/prime-factors/prime_factors.rb
+++ ruby/prime-factors/prime_factors.rb
@@ -1,11 +1,20 @@
-require 'prime'
-
 module PrimeFactors
   def self.for(number)
-    Prime.each(number).each_with_object([]) do |prime, factors|
+    divisors.each_with_object([]) do |d, factors|
       break factors if number == 1
-      factors << number && (break factors) if Math.sqrt(number) < prime
-      factors << prime && number /= prime while number % prime == 0
+      factors << number && (break factors) if Math.sqrt(number) < d
+      factors << d && number /= d while number % d == 0
+    end
+  end
+
+  class << self
+    private
+
+    def divisors
+      return enum_for(:divisors) unless block_given?
+      yield 2
+      n = 1
+      loop { yield n += 2 }
     end
   end
 end
