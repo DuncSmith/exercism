35cda7bf17ac30722b61e9a04e516db0ed3386ac
Trevor Barnett <mr.ullet@gmail.com>
2015-06-06 17:15:07 +0100

    :construction_worker: Ruby Trinary - Generalised for base-n, 2 <= n <= 10

--- ruby/trinary/trinary.rb
+++ ruby/trinary/trinary.rb
@@ -1,25 +1,43 @@
-class Trinary
-  def initialize(trinary)
-    @trinary = trinary
+class BaseN
+  def initialize(base_value, base)
+    @base_value = base_value
+    @base = base
+    validate_initialization
   end
 
   def to_decimal
-    valid ? trit_values.reduce(:+) : 0
+    valid ? digit_values.reduce(:+) : 0
   end
 
   private
 
-  attr_reader :trinary
+  attr_reader :base_value, :base
 
   def valid
-    trinary.chars.all? { |c| %w(0 1 2).include?(c) }
+    base_value.chars.any? &&
+      base_value.chars.all? { |c| valid_chars.include?(c) }
   end
 
-  def trit_values
-    trits.each_with_index.map { |b, n| b * 3**n }
+  def valid_chars
+    (0...base).map(&:to_s)
   end
 
-  def trits
-    trinary.reverse.chars.map(&:to_i)
+  def digit_values
+    digits.each_with_index.map { |b, n| b * base**n }
+  end
+
+  def digits
+    base_value.reverse.chars.map(&:to_i)
+  end
+
+  def validate_initialization
+    return unless base < (min = 2) || base > (max = 10)
+    fail(ArgumentError, "Base must be between #{min} and #{max}")
+  end
+end
+
+class Trinary < BaseN
+  def initialize(trinary)
+    super(trinary, 3)
   end
 end
--- ruby/trinary/trinary_test.rb
+++ ruby/trinary/trinary_test.rb
@@ -39,4 +39,36 @@ class TrinaryTest < Minitest::Test
   def test_invalid_trinary_is_decimal_0
     assert_equal 0, Trinary.new('carrot').to_decimal
   end
+
+  def test_base_4_1_is_decimal_1
+    assert_equal 1, BaseN.new('1', 4).to_decimal
+  end
+
+  def test_base_6_135_is_decimal_59
+    assert_equal 59, BaseN.new('135', 6).to_decimal
+  end
+
+  def test_base_2_1110000110_is_decimal_902
+    assert_equal 902, BaseN.new('1110000110', 2).to_decimal
+  end
+
+  def test_invalid_base_3_is_decimal_0
+    assert_equal 0, BaseN.new('444', 3).to_decimal
+  end
+
+  def test_error_if_base_less_than_2
+    assert_raises ArgumentError do
+      BaseN.new('0', 1)
+    end
+  end
+
+  def test_error_if_base_greater_than_10
+    assert_raises ArgumentError do
+      BaseN.new('A', 11)
+    end
+  end
+
+  def test_invalid_empty_base_value_is_decimal_0
+    assert_equal 0, BaseN.new('', 2).to_decimal
+  end
 end
