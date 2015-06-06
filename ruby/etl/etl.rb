e916572886f067872316f2cd33be1e8d392c0e73
Trevor Barnett <mr.ullet@gmail.com>
2015-06-06 16:17:15 +0100

    :construction_worker: Ruby ETL - much simplified

--- ruby/etl/etl.rb
+++ ruby/etl/etl.rb
@@ -1,23 +1,7 @@
 class ETL
-  def self.transform(score_letter_hash)
-    ETL.new(score_letter_hash).letter_score_hash
-  end
-
-  def initialize(score_letter_hash)
-    @score_letter_hash = score_letter_hash
-  end
-
-  def letter_score_hash
-    letter_score_pairs.reduce(:merge)
-  end
-
-  private
-
-  attr_reader :score_letter_hash
-
-  def letter_score_pairs
-    score_letter_hash.map do |score, letters|
-      Hash[letters.map {|letter| [letter.downcase, score]}]
-    end
+  def self.transform(source)
+    source.flat_map do |score, letters|
+      letters.map { |letter| [letter.downcase, score] }
+    end.to_h
   end
 end
