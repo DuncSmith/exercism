276825b49dff04ecaeb7f9498efca2733ac24b3d
Trevor Barnett <mr.ullet@gmail.com>
2015-06-19 17:03:43 +0100

    :construction_worker: Ruby Palindrome Products - much faster, much messier

--- ruby/palindrome-products/palindrome_products.rb
+++ ruby/palindrome-products/palindrome_products.rb
@@ -1,24 +1,49 @@
 require 'ostruct'
 
 class Palindromes
+  attr_reader :largest, :smallest
+
   def initialize(options)
     @min_factor = options[:min_factor] || 1
     @max_factor = options[:max_factor] || 1
   end
 
   def generate
-    @palindromes ||=
-      products.select { |p, _| palindrome?(p) }
-      .sort { |a, b| a[0] <=> b[0] }
-      .map { |v, f| OpenStruct.new(value: v, factors: f) }
-  end
+    smallest_value = max_factor**2
+    smallest_factors = []
+    min_factor.upto(max_factor).each do |m|
+      break if m**2 > smallest_value
+      m.upto(max_factor).each do |n|
+        product = m * n
+        break if product > smallest_value
+        next unless palindrome?(product)
+        if smallest_value > product
+          smallest_value = product
+          smallest_factors = []
+        end
+        smallest_factors << [m, n]
+      end
+    end
+    @smallest =
+      OpenStruct.new(value: smallest_value, factors: smallest_factors)
 
-  def largest
-    palindromes.last
-  end
-
-  def smallest
-    palindromes.first
+    largest_value = min_factor**2
+    largest_factors = []
+    max_factor.downto(min_factor).each do |m|
+      break if m**2 < largest_value
+      m.downto(min_factor).each do |n|
+        product = m * n
+        break if product < largest_value
+        next unless palindrome?(product)
+        if largest_value < product
+          largest_value = product
+          largest_factors = []
+        end
+        largest_factors << [n, m]
+      end
+    end
+    @largest =
+      OpenStruct.new(value: largest_value, factors: largest_factors)
   end
 
   private
