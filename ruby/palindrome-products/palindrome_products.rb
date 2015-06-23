9af3df8e77d7192c4a016b2bd95ae773b931df02
Trevor Barnett <mr.ullet@gmail.com>
2015-06-23 13:49:14 +0100

    :construction_worker: Ruby Palindrome Products - remove redundant method

--- ruby/palindrome-products/palindrome_products.rb
+++ ruby/palindrome-products/palindrome_products.rb
@@ -17,10 +17,6 @@ class Palindromes
 
   attr_reader :min_factor, :max_factor, :palindromes
 
-  def products
-    pairs.reduce({}) { |a, (m, n)| (a[m * n] ||= []) << [m, n] && a }
-  end
-
   def pairs
     (min_factor..max_factor).to_a.repeated_combination(2)
   end
